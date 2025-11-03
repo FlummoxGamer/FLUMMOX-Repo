869622f52c3a8a9fa89aca8f765c7346039e5077
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-04 02:07:38 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,24 +2,15 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
-        // FIX: Use the correct GitHub Packages URL
-        maven {
-            name = "cloudstream"
-            url = uri("https://maven.pkg.github.com/recloudstream/cloudstream")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // FIX: Add the dependency repository here too
         maven {
             name = "cloudstream"
             url = uri("https://maven.pkg.github.com/recloudstream/cloudstream")
