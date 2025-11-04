626a53f30cb0afc353de8bf638d07f785b508002
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-05 02:13:01 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,37 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1
+
+android {
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
     }
+    defaultConfig {
+        android.buildFeatures.buildConfig=true
+    }
+}
+
+cloudstream {
+    language = "en"
+    description = "Multi-source provider with AllMovieLand and more"
+    authors = listOf("FlummoxGamer")
+
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Cartoon"
+    )
+
+    requiresResources = true
+}
+
+dependencies {
+    val cloudstream by configurations
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.browser:browser:1.9.0")
+    cloudstream("com.lagradost:cloudstream3:pre-release")
 }
