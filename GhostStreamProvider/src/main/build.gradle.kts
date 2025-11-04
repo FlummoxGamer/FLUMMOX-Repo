4a9a4da0f5d0492750be436355425c1429ed38cd
FlummoxGamer <140507968+FlummoxGamer@users.noreply.github.com>
2025-11-05 03:06:09 +0530

    Update build.gradle.kts

--- GhostStreamProvider/src/main/build.gradle.kts
+++ GhostStreamProvider/src/main/build.gradle.kts
@@ -18,8 +18,6 @@ repositories {
 dependencies {
     implementation("com.lagacy:ext-api:master-SNAPSHOT")
     implementation(kotlin("stdlib-jdk8"))
-    
-    // Add these dependencies for HTTP requests and HTML parsing
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("org.jsoup:jsoup:1.16.1")
 }
@@ -27,25 +25,4 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-// Create a fat JAR with all dependencies
-tasks.register<Jar>("fatJar") {
-    archiveBaseName.set("GhostStreamProvider")
-    archiveVersion.set("1.0.0")
-    
-    from(sourceSets.main.get().output)
-    dependsOn(configurations.runtimeClasspath)
-    
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
-    
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-tasks.register<Jar>("fatJar") {
-    archiveClassifier.set("all")
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    with(tasks.jar.get())
 }
