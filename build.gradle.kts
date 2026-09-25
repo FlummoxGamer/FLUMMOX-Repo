846b08c3017c5afe1486afa25ac545b552fca684
sayFLUMMOX <140507968+FlummoxGamer@users.noreply.github.com>
2026-09-25 08:50:42 +0530

    Update Gradle plugin dependency for ReCloudStream

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream.gradle:com.lagradost.cloudstream3.gradle.gradle.plugin:-SNAPSHOT")
     }
 }
 
